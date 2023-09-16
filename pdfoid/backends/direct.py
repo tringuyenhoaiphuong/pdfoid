4aa4919ac0add6c622383da655a9314143b398de
Canh Le My <lemycanh@gmail.com>
2023-09-16 15:34:32 +0700

    * fix

--- pdfoid/backends/direct.py
+++ pdfoid/backends/direct.py
@@ -14,7 +14,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.chrome.service import Service
 
 class DirectSeleniumBackend(object):
     def __init__(self):
